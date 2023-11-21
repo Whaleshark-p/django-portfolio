02ad6f81a858606a1ec314cb0ea5f98ba8f6e8bd
Pieter Etman <psetman@gmail.com>
2023-11-21 12:04:15 +0100

    added and configured allowed hosts

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 from decouple import config
 
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
