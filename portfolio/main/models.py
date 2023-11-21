67068d65a75e684ded2805a8c6353caa84d7689d
Pieter Etman <psetman@gmail.com>
2023-11-21 09:33:59 +0100

    first commit

---
+++ portfolio/main/models.py
@@ -0,0 +1,16 @@
+import os
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=200)
+    thumbnail = models.FilePathField(path='static/main')
+    text = models.CharField(max_length = 4000)
+    hyperlink = models.URLField(max_length = 200, blank=True)
+
+    def __str__(self):
+        return self.title
+    
